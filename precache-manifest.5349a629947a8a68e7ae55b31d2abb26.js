6c2d38c6b93223dca9961146b9bcb450c77570a5
Alexandre Kone <kone.alexandr@gmail.com>
2019-03-21 22:11:50 +0100

    init commit

---
+++ precache-manifest.5349a629947a8a68e7ae55b31d2abb26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "7a214c01410f26873ee9",
+    "url": "/static/js/main.473a3bdf.chunk.js"
+  },
+  {
+    "revision": "a023eca1a2f1ab2e7bbe",
+    "url": "/static/js/2.74b4be5e.chunk.js"
+  },
+  {
+    "revision": "7a214c01410f26873ee9",
+    "url": "/static/css/main.4115b9dd.chunk.css"
+  },
+  {
+    "revision": "ff43f4a388b4f2b3d0bf834b88700593",
+    "url": "/index.html"
+  }
+];
